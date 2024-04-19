479487b9564192d05da287e63d61a78d439dff3a
n-hidaka <85332716+n-hidaka@users.noreply.github.com>
2024-04-19 11:17:57 +0900

    chore: update validation of room name

--- src/shared/validate.ts
+++ src/shared/validate.ts
@@ -5,8 +5,15 @@ export const isValidRoomType = (type: string): boolean => {
   return false;
 };
 
-export const maxRoomIdLength = 16;
-export const roomIdRe = "^[0-9a-z_-]{4,16}$";
+// Channel、Memberのnameとして使用可能な文字種
+// from https://skyway.ntt.com/en/docs/user-guide/commons/quotas-and-limits/
+// > Allowed characters: Only characters composed of a-z, A-Z, 0-9, -, ., _, %, *
+// - * is allowed but not used
+// > Number of characters: 1~128 characters (0 characters are not allowed)
+// - 4 characters or more to reduce the possibility of unexpected users using the same Room Id
+// - 120 characters or less to be added prefix 'P2P_' or 'SFU_'
+export const maxRoomNameLength = 120;
+export const roomIdRe = "^[a-zA-Z0-9-._%]{4,120}$";
 export const isValidRoomId = (name: string): boolean => {
   return new RegExp(roomIdRe).test(name);
 };
